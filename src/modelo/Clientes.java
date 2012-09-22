ec45ef4773f9c506b32191cfb052aa8503306971
Marcio Henrique Soares <marciohsoares@bol.com.br>
2012-09-22 17:48:05 -0300

    teste 2
    Criacao de estrutura do programa para teste

--- src/modelo/Clientes.java
+++ src/modelo/Clientes.java
@@ -1,5 +1,46 @@
 package modelo;
 
 public class Clientes {
+	
+	private String nome;
+	private Float n1,n2,n3,n4;
+	
+	public Clientes() {
+		super();
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Float getN1() {
+		return n1;
+	}
+	public void setN1(Float n1) {
+		this.n1 = n1;
+	}
+	public Float getN2() {
+		return n2;
+	}
+	public void setN2(Float n2) {
+		this.n2 = n2;
+	}
+	public Float getN3() {
+		return n3;
+	}
+	public void setN3(Float n3) {
+		this.n3 = n3;
+	}
+	public Float getN4() {
+		return n4;
+	}
+	public void setN4(Float n4) {
+		this.n4 = n4;
+	}
 
+	
+	
 }
